50fb8272c920781990423b3bc128367037cf7919
byidnekachew <byidnekachew@my.waketech.edu>
2026-04-14 12:16:10 +0000

    Fixed erros in code for validate_ticket

--- test_ticket_validator.py
+++ test_ticket_validator.py
@@ -1,3 +1,4 @@
+import pytest
 from ticket_validator import validate_ticket, get_ticket_tier, calculate_total
 
 def test_correct_ticket_start():
--- ticket_validator.py
+++ ticket_validator.py
@@ -2,7 +2,7 @@ def validate_ticket(code):
     if not(isinstance(code, str)):
         raise TypeError("Code not inputted in string format")
     else:
-        return len(code) == 8 and code[0:2] == "TK" and if code[2:9].isdigit
+        return len(code) == 8 and code[0:2] == "TK" and code[2:9].isdigit
 
 
 def get_ticket_tier(code):
